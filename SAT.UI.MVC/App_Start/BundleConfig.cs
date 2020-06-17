74eb8b00ec19e1bf474a0e9422709cc7a8ec90b4
Adriana223 <Lamb_is_here@yahoo.com>
2020-06-17 11:26:16 -0500

    Convert template, 75% on contact, put HTML in ContactAJAX commit

--- SAT.UI.MVC/App_Start/BundleConfig.cs
+++ SAT.UI.MVC/App_Start/BundleConfig.cs
@@ -1,6 +1,6 @@
 ﻿using System.Web.Optimization;
 
-namespace IdentitySample
+namespace SAT.UI.MVC
 {
     public class BundleConfig
     {
--- SAT.UI.MVC/Controllers/HomeController.cs
+++ SAT.UI.MVC/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System.Web.Mvc;
+﻿using SAT.UI.MVC.Models;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
 
-namespace IdentitySample.Controllers
+namespace SAT.UI.MVC.Controllers
 {
     public class HomeController : Controller
     {
@@ -25,6 +29,34 @@ namespace IdentitySample.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
-        }
+        }//end GEtContact
+
+        [HttpPost]
+        public JsonResult ContactAjax(ContactViewModel cvm)
+        {
+            string body = $"{cvm.Name} has sent you: <br />{cvm.Message} <strong>from the email address:</strong> {cvm.Email}";
+
+            MailMessage m = new MailMessage("no-reply@adrianalamb.com", "Lamb_is_here@yahoo.com", cvm.Subject, cvm.Message);
+
+            m.IsBodyHtml = true;
+
+            //reply to the Person who filled out the form, not your domain email
+            m.ReplyToList.Add(cvm.Email);
+
+            SmtpClient client = new SmtpClient("mail.adrianalamb.com");
+            client.Credentials = new NetworkCredential("no-reply@adrianalamb.com", "@aKBXqk3Nbb7Y");
+            client.Port = 8889;
+
+            try
+            {
+                client.Send(m);
+            }
+            catch (Exception ex)
+            {
+
+                ViewBag.Message = ex.StackTrace;
+            }//end catch
+            return Json(cvm);
+        }//end PostContact
     }
 }
